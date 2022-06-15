b859bb0605c39efe200c6a164767ebca4c7fc47e
Dsfr62 <saviodudu79@gmail.com>
2022-06-15 14:38:42 -0300

    Implemented go back option list for md view, started fscreen view

--- src/components/navbar/navOptionsMd/optionList/index.jsx
+++ src/components/navbar/navOptionsMd/optionList/index.jsx
@@ -1,13 +1,17 @@
 import React from "react";
+import { CgCloseO } from 'react-icons/cg';
 
 const OptionList = ({options}) => {
     return (
-        <div className="bg-customOrange rounded-lg">
-            <ul className="text-black font-semibold">
-                {options.map(option => 
-                    <li className="p-2 border-b border-black hover:bg-customGreen hover:rounded-sm ease-linear transition-all"><a href={option.link}>{option.text}</a></li>
-                )}
-            </ul>
+        <div>
+            <CgCloseO size={15} className="mb-1.5"/>
+            <div className="bg-customOrange rounded-lg">  
+                <ul className="text-black font-semibold">
+                    {options.map(option => 
+                        <li className="p-2 border-b border-black hover:bg-customGreen hover:rounded-sm ease-linear transition-all"><a href={option.link}>{option.text}</a></li>
+                    )}
+                </ul>
+            </div>
         </div>
     );
 };
